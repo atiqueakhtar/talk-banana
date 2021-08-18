63a9244f763aa79fa16f2be64e5a2602bc4c4c20
Atique Akhtar <atiqueakhtar786@gmail.com>
2021-08-18 16:57:38 +0530

    feat: wired app.js

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var buttonObj = document.querySelector("#button-en");
-var inputText = document.querySelector("#input-text");
-var outputText = document.querySelector("#output-text");
+var inputTextObj = document.querySelector("#input-text");
+var outputTextObj = document.querySelector("#output-text");
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationalUrl(text){
+    return url + "?text=" + text;
+}
+function errorHandler(error){
+    console.log("error occured", error)
+}
 
 buttonObj.addEventListener("click", function clickHandler(){
-    outputText.innerText = "Hi Atique! " + inputText.value;
+    var inputText = inputTextObj.value;
+    var textUrl = getTranslationalUrl(inputText);
+    fetch(textUrl)
+    .then(function jsonify(response){ return response.json(); })
+    .then(function translate(json){ return json.contents.translated; })
+    .then(function displayTanslated(text){ outputTextObj.innerText = text; })
+    .catch(errorHandler)
 });
 
